969822e36389410ea6fec4395d0c1b2fd713ef62
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-09-08 01:55:27 +0300

    Another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
 import ForgotPassword from "./components/User/ForgotPassword";
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/ManageMyLiveBK/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// Setăm baza în funcție de mediul de rulare
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/ManageMyLiveBK/" : "/", // Folosim baza doar în producție
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
